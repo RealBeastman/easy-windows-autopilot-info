6146773bc58e2f9d5505a66d570093b153122782
Joshua Eastman <joshuabeastman@gmail.com>
2022-06-10 18:51:17 -0500

    Add files via upload

--- Autopilot.bat
+++ Autopilot.bat
@@ -1,4 +1,4 @@
 
 
-:: Powershell cmdlets. Creates HWID directory, declares execution policy, installs necessary packages, runs autopilot PS script, and creates a serial specific .csv file.
-REM Powershell "& {set-executionpolicy -scope Process -executionpolicy remotesigned -force; new-item -type directory -path 'C:\HWID'; set-location -path "C:\HWID"; $env:Path += ';C:\Program Files\WindowsPowerShell\Scripts'; install-packageprovider -name NuGet -requiredversion 2.8.5.201 -force; install-package -name get-windowsautopilotinfo -force; get-windowsautopilotinfo -outputfile serialRename.csv;}"
+:: **POWERSHELL CMDLETS** Creates HWID directory, declares execution policy, installs necessary packages, runs autopilot PS script, and creates a serial specific .csv file.
+Powershell "& {set-executionpolicy -scope Process -executionpolicy remotesigned -force; new-item -type directory -path 'C:\HWID'; set-location -path "C:\HWID"; $env:Path += ';C:\Program Files\WindowsPowerShell\Scripts'; install-packageprovider -name NuGet -requiredversion 2.8.5.201 -force; install-package -name get-windowsautopilotinfo -force; get-windowsautopilotinfo -outputfile serialRename.csv;}"
